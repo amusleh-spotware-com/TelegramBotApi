c5273d331693673b6cdd6edb942b0dc0a13d29f2
Ahmad Noman Musleh <afhackermubasher@gmail.com>
2018-10-19 10:34:08 +0300

    Remote repo changed

--- TelegramBotApi/Properties/AssemblyInfo.cs
+++ TelegramBotApi/Properties/AssemblyInfo.cs
@@ -1,14 +1,14 @@
 ﻿using System.Reflection;
-using System.Runtime.CompilerServices;
+using System.Resources;
 using System.Runtime.InteropServices;
 
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("TelegramBotApi")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("A lightweight version of Telegram.Bot, its fully customized for special use case")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("AlgoDeveloper")]
 [assembly: AssemblyProduct("TelegramBotApi")]
 [assembly: AssemblyCopyright("Copyright ©  2018")]
 [assembly: AssemblyTrademark("")]
@@ -32,5 +32,6 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("1.0.0.1")]
+[assembly: AssemblyFileVersion("1.0.0.1")]
+[assembly: NeutralResourcesLanguage("en-US")]
